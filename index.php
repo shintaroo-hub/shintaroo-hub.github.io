b5eec8d9838655267a38c848689e8f7322785cf8
Shinta Room <koalablank@gmail.com>
2018-06-21 20:23:45 +0800

    changed index file to php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include("pages/base.html") ?>
+<body class="page-md page-header-fixed page-quick-sidebar-over-content">
+    <div id="body_content">
+        <section id="main_header">
+            <nav>
+                <ul id="navbar-top-left">
+                    <li>BLNK</li>
+                </ul>
+                <ul id="navbar-top-right">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Works</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+
+        </section>  
+
+        <section id="home" class="flex">
+            <div class="flex">
+                <div class="text">
+                    Hello!<br> 
+                    This is a test website created on github
+                </div>           
+            </div>
+        </section>
+    </div>
+</body>
